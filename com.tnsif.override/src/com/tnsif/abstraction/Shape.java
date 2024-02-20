c6ae1172745e1904e63bc09f2843f57a1d0aa591
Dell <Dell@DESKTOP-8J1H4P2>
2024-02-20 15:16:20 +0530

    Second commit

---
+++ com.tnsif.override/src/com/tnsif/abstraction/Shape.java
@@ -0,0 +1,19 @@
+package com.tnsif.abstraction;
+
+public abstract class Shape {
+	
+	
+	protected float area;  //variable
+	
+	//abstract method
+	
+	abstract void calarea();
+	
+	
+	
+	//concrete method
+	void show() {
+		System.out.println("area of a shape"+area);
+	}
+
+}
