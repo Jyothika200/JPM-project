a5bc7b7ff4f59fdac6ae57e666f551acbddecf52
Dell <Dell@DESKTOP-8J1H4P2>
2024-03-08 15:11:05 +0530

    lambda

---
+++ com.tnsif.override/src/com/tnsif/lambdaexpression/Cube.java
@@ -0,0 +1,11 @@
+package com.tnsif.lambdaexpression;
+@FunctionalInterface
+
+public interface Cube {
+	
+	
+	
+	int calculate(int a);
+	
+
+}
---
+++ com.tnsif.override/src/com/tnsif/lambdaexpression/LambdaCollection.java
@@ -0,0 +1,20 @@
+package com.tnsif.lambdaexpression;
+import java.util.ArrayList;
+//to demonstrate the lambda expression for collections
+
+public class LambdaCollection {
+	public static void main(String[] args) {
+		ArrayList <Integer> li=new ArrayList<Integer>();
+		li.add(2);
+		li.add(22);
+		li.add(8);
+		li.add(5);
+		//for each loop
+		//for(Integer ele:li) {
+		//System.out.println(ele);
+		// }
+		//lambda expression
+		li.forEach(element    ->System.out.println(element));
+	}
+
+}
